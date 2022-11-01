2bc2a17c51561ac231028089356aa161856fc992
Irinni <irinnilvova@gmail.com>
2022-11-01 21:35:56 +0200

    Create Circle.cs
    Added circle class with area calculations

---
+++ FigureCalculations/Figures/Circle.cs
@@ -0,0 +1,29 @@
+﻿namespace FigureCalculations.Figures
+{
+    // Т.к. нет требования к изменению параметров треугольника - стороны треугольника можно задать только при инициализации
+    // Это позволит произволит вычислять дополнительные атрибуты (площадь) только 1 раз при первом обращении,
+    // что ускоряет выполнение кода. Также данное решение упрощает поддержку и чтение кода.
+    public class Circle : IFigure
+    {
+        private const string errorNegativeRadiusMessage = "Radius can't be negative";
+        public double Radius { get; private set; }
+        private double? _area = null;
+
+        public Circle(double r)
+        {
+            if (r < 0)
+                throw new ArgumentException(errorNegativeRadiusMessage);
+            else
+                Radius = r;
+        }
+        public double Area
+        {
+            get
+            {
+                if (!_area.HasValue)
+                    _area = Math.PI * Math.Pow(Radius, 2);
+                return _area.Value;
+            }
+        }
+    }
+}
